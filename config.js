e30d6561b1499ca03253555f7f6295e66051a855
xiabin1235910 <416854144@qq.com>
2016-08-05 17:12:22 +0800

    update config

---
+++ config.js
@@ -0,0 +1,19 @@
+//test config
+var config = {};
+
+config.port = 3000;
+
+var db_server = "localhost";
+var db_user = "root";
+var db_password = "";
+
+config.serviceServerDB =
+{
+    connectionLimit: 5,
+    host: db_server,
+    database: "sd_servicedb",
+    user: db_user,
+    password: db_password
+};
+
+module.exports = config;
